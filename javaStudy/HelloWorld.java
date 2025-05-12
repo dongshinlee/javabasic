9a6b94c3ddccd6760a1539b1c090999cf86888ae
dongshinlee <fullstack.leened@naver.com>
2025-05-12 17:44:26 +0900

    init commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕, 난 이동신 이야.");
+	}
+	
+	// 문자셋 설정: 이 설정이 각각 틀리면, 한글 데이터나, 주석이 깨질 수 있음
+	// 때문에 반드시 문자셋 설정을 초기에 해둬야 나중에 후회 없음
+	// 모든 문자셋은 UTF-8로 설정
+	
+}
